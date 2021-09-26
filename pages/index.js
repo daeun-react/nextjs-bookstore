60c1a5696657f731942fbb0b055bc28e1da185fc
daeun-react <smile_daeun@naver.com>
2021-09-26 22:59:01 +0900

    feat : pages/index.js 파일 내용 수정

--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,27 @@
-import Head from "next/head";
-import Image from "next/image";
 import styled from "styled-components";
-
-const Container = styled.div``;
+import { FiSmile } from "react-icons/fi";
 
 export default function Home() {
   return (
     <Container>
-      <h1>BOOK STORE</h1>
+      <FiSmile color="gray" size={32} />
+      <h1>도서 검색 사이트</h1>
+      <h2>WELCOME!</h2>
     </Container>
   );
 }
+
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  height: 100vh;
+  color: gray;
+  font-size: 16px;
+  line-height: 1.5;
+
+  svg {
+    margin: 10px;
+  }
+`;
