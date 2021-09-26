ad7bbd7d937879b8708a51b643a14aa56b511890
daeun-react <smile_daeun@naver.com>
2021-09-26 17:51:33 +0900

    feat : styled-component GlobalStyle 설정 및 폰트 적용하기

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,13 @@
-import '../styles/globals.css'
+import App from "next/app";
+import GlobalStyle from "../styles/GlobalStyle";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <GlobalStyle />
+      <Component {...pageProps} />
+    </>
+  );
 }
 
-export default MyApp
+export default MyApp;
